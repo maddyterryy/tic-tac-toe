94541b48879ae5539ea2eca1c517c3afa0a3b112
maddyterryy <madterry2001@gmail.com>
2024-04-08 19:52:10 -0700

    removed last commit from Square function

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
+import { useState } from 'react';
+
 export default function Board() {
+  //attaches state (null) to each square so board can keep track of
+  //which square has been clicked 
+  const[squares, setSquares] = useState(Arrays(9).fill(null));
+
+  //the visible game board 
   return (
     <>
   <div className="board-row">
-  <Square value="1"/>
-  <Square value="2"/>
-  <Square value="3"/>
+  <Square value={squares[0]}/>
+  <Square value={squares[1]}/>
+  <Square value={squares[2]}/>
   </div>
   <div className="board-row">
-  <Square value="4"/>
-  <Square value="5"/>
-  <Square value="6"/>
+  <Square value={squares[3]}/>
+  <Square value={squares[4]}/>
+  <Square value={squares[5]}/>
   </div>
   <div className="board-row">
-  <Square value="7"/>
-  <Square value="8"/>
-  <Square value="9"/>
+  <Square value={squares[6]}/>
+  <Square value={squares[7]}/>
+  <Square value={squares[8]}/>
   </div>
   </>
   );
 }
 
-function Square( {value} ){
-  function handleClick(){
-    console.log('clicked! ')
-  }
-
-
-  return <button className="square" onClick={handleClick}>
-    {value}</button>;
+//each square receives a value prop that
+//will be 'X', 'O', or 'null' for empty squares
+function Square({value}){
+  return( <button className="square">{value}</button>);
 
 }
