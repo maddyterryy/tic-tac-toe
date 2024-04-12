ffeebf97d8d32db57e9b2d1497f0050193ae052a
maddyterryy <madterry2001@gmail.com>
2024-04-11 23:02:26 -0700

    lifted state

--- src/App.js
+++ src/App.js
@@ -5,23 +5,35 @@ export default function Board() {
   //which square has been clicked 
   const[squares, setSquares] = useState(Arrays(9).fill(null));
 
+//creates copy of squares array using .slice() and updates
+//the value of an index, then calls setSquares to trigger re-rendering
+//of the components that use 'squares' and its child components
+function handleClick(i){
+const nextSquares = squares.slice();
+nextSquares[i] = 'X';
+setSquares(nextSquares);
+}
+
+
   //the visible game board 
+  //added arrow function to onSquareClick which means
+  //the code after '=>' will run after square is clicked
   return (
     <>
   <div className="board-row">
-  <Square value={squares[0]}/>
-  <Square value={squares[1]}/>
-  <Square value={squares[2]}/>
+  <Square value={squares[0]} onSquareClick={() => handleClick(0)} /> 
+  <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+  <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
   </div>
   <div className="board-row">
-  <Square value={squares[3]}/>
-  <Square value={squares[4]}/>
-  <Square value={squares[5]}/>
+  <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+  <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+  <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
   </div>
   <div className="board-row">
-  <Square value={squares[6]}/>
-  <Square value={squares[7]}/>
-  <Square value={squares[8]}/>
+  <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+  <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+  <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
   </div>
   </>
   );
@@ -30,6 +42,8 @@ export default function Board() {
 //each square receives a value prop that
 //will be 'X', 'O', or 'null' for empty squares
 function Square({value}){
-  return( <button className="square">{value}</button>);
+  return( <button className="square">
+    {value}
+    </button>);
 
 }
