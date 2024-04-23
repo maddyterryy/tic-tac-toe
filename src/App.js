6885717b7481234b490f608cad340653cf36798b
maddyterryy <madterry2001@gmail.com>
2024-04-23 16:00:47 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import { useState } from 'react';
 
+//each square receives a value prop that
+//will be 'X', 'O', or 'null' for empty squares
+function Square({value, onSquareClick}){
+  return( <button className="square" onClick={onSquareClick}>
+    {value}
+    </button>);
+
+}
+
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   //attaches state (null) to each square so board can keep track of
   //which square has been clicked 
-  const[squares, setSquares] = useState(Arrays(9).fill(null));
+  const[squares, setSquares] = useState(Array(9).fill(null));
 
 //creates copy of squares array using .slice() and updates
 //the value of an index, then calls setSquares to trigger re-rendering
 //of the components that use 'squares' and its child components
 function handleClick(i){
+//checks if player has one or has already clicked a square
+ if (squares[i] || calculateWinner(squares)){
+  return;
+ }
 const nextSquares = squares.slice();
-nextSquares[i] = 'X';
+//xIsNext function will flip depending on whose turn it is
+if (xIsNext) {
+  nextSquares[i] = "X";
+}
+else{
+  nextSquares[i] = "O";
+}
 setSquares(nextSquares);
+setXIsNext(!xIsNext);
 }
 
-
+//lets players know when game is over or whose turn it is
+const winner = calculateWinner(squares);
+let status;
+if (winner){
+  status = "Winner: " + winner;
+} else {
+  status = "Next player: " + (xIsNext ? "X" : "O");
+}
   //the visible game board 
   //added arrow function to onSquareClick which means
   //the code after '=>' will run after square is clicked
@@ -39,11 +67,23 @@ setSquares(nextSquares);
   );
 }
 
-//each square receives a value prop that
-//will be 'X', 'O', or 'null' for empty squares
-function Square({value}){
-  return( <button className="square">
-    {value}
-    </button>);
-
+//function that checks for a winner and returns 'x', 'o', or 'null' 
+function calculateWinner(squares){
+const lines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+for (let i = 0; i < lines.length; i++) {
+  const [a, b, c] = lines[i];
+  if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+    return squares[a];
+  }
+}
+return null;
 }
